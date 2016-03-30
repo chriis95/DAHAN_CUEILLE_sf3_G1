14135fb4eba09ca35298b1b0dc29230e21431eb3
Robin <godartrobin@gmail.com>
2016-03-30 08:37:10 +0200

    Init

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,26 +3,11 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello-world")
-     */
     public function indexAction()
     {
-        return new Response(
-            '<html><body>Hello world</body></html>'
-        );
-    }
-
-    /**
-     * @Route("/list")
-     */
-    public function listAction()
-    {
-
+        return $this->render('AppBundle:Default:index.html.twig');
     }
 }
